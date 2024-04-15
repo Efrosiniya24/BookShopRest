8325458a144ae205a2b0ad8eac634cbae32d5444
frosya <efrosiniya24@gmail.com>
2024-04-15 10:10:04 +0300

    UserRepository

---
+++ src/main/java/BookShopRest/BookShopRest/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package BookShopRest.BookShopRest.repositories;
+
+import BookShopRest.BookShopRest.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
